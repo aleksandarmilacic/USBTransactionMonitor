9e74f1a40d53fe2dd4ca8549e36dc980f00a02e6
amilacic <aleksandar.milacic@gmail.com>
2023-07-09 19:22:37 +0200

    Add project files.

---
+++ USBTransactionMonitor/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Management;
+
+namespace USBTransactionMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Subscribe to USB device insertion and removal events
+            var watcher = new ManagementEventWatcher();
+            var query = new WqlEventQuery("SELECT * FROM __InstanceOperationEvent WITHIN 1 WHERE TargetInstance ISA 'Win32_USBControllerDevice'");
+            watcher.EventArrived += USBEventHandler;
+            watcher.Query = query;
+            watcher.Start();
+
+            Console.WriteLine("USB Transaction Monitor running. Press any key to exit...");
+            Console.ReadKey();
+
+            // Stop monitoring
+            watcher.Stop();
+        }
+
+        static void USBEventHandler(object sender, EventArrivedEventArgs e)
+        {
+            var instance = (ManagementBaseObject)e.NewEvent["TargetInstance"];
+            var eventType = e.NewEvent.ClassPath.ClassName;
+
+            if (eventType.Equals("__InstanceCreationEvent"))
+            {
+                // USB device inserted
+                var deviceId = (string)instance["Dependent"];
+                Console.WriteLine("USB Device Inserted:");
+                Console.WriteLine($"Device ID: {deviceId}");
+                Console.WriteLine();
+            }
+            else if (eventType.Equals("__InstanceDeletionEvent"))
+            {
+                // USB device removed
+                var deviceId = (string)instance["Dependent"];
+                Console.WriteLine("USB Device Removed:");
+                Console.WriteLine($"Device ID: {deviceId}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,37 @@
+# USB Transaction Monitor
+
+The USB Transaction Monitor is a simple C# application that monitors the transactions of a USB port. It detects and displays notifications when USB devices are inserted or removed from the system.
+
+## Prerequisites
+
+- .NET Framework (version X.X or later) installed on the target machine.
+
+## Getting Started
+
+1. Clone or download the project files to your local machine.
+
+2. Open the project in your preferred C# development environment (e.g., Visual Studio).
+
+3. Build the project to compile the source code.
+
+4. Run the application, and it will start monitoring the USB port.
+
+5. Whenever a USB device is inserted or removed, the application will display a notification with the device ID.
+
+6. Press any key to stop the monitoring and exit the application.
+
+## Important Note
+
+- To access USB device events, the application may need to be run with administrative privileges.
+
+## Contributing
+
+Contributions to this project are welcome! If you find any issues or have suggestions for improvements, feel free to submit a pull request or open an issue.
+
+When contributing, please ensure that your code follows the established coding style and practices. Additionally, provide clear and concise commit messages to facilitate the review process.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+---
